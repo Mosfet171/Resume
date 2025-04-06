eeb4db9696c75c87738da783889cc0c83c699e3a
Mosfet171 <arkadym@live.be>
2025-04-06 21:26:35 +0200

    Ext JS + Minor Fixes

---
+++ script.js
@@ -0,0 +1,23 @@
+function showDescription(id) {
+    document.querySelectorAll('.description').forEach(desc => {
+        desc.classList.remove('active');
+    });
+    document.getElementById(id).classList.add('active');
+}
+
+function toggleSection(section) {
+    const sectionElement = document.getElementById(section);
+    sectionElement.style.display = sectionElement.style.display === 'none' ? 'block' : 'none';
+
+}
+
+function toggleSectionTwoCols(sectionId) {
+    let section = document.getElementById(sectionId);
+    if (section.classList.contains("hidden")) {
+        section.classList.remove("hidden");
+        section.classList.add("visible");
+    } else {
+        section.classList.remove("visible");
+        section.classList.add("hidden");
+    }
+}
